0b6ff0c91568a87e6d4ce645c05b3ae4d135078f
navjot <nsn8@sfu.ca>
2025-02-10 21:43:43 -0800

    Added the functionality to contact form using js script

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,37 @@ if(navClose){
         navMenu.classList.remove('show-menu')
     })
 }
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_v0yvpg5', 'template_1qdt58c', '#contact-form', '50jspKStyBIdh91No')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅';
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000);
+
+            // Clear input fields
+            contactForm.reset();
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌';
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = '';
+            }, 5000);
+
+        });
+};
+
+contactForm.addEventListener('submit', sendEmail);
